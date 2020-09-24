493199433a30800252cc8050fc7986e92edb4318
Marcella Luz Ash <marcella@ash-photos.com>
2020-09-23 23:10:00 -0700

    model for db

--- models/exercise.js
+++ models/exercise.js
@@ -1,16 +1,49 @@
 const { model, Schema } = require('mongoose')
 
 const Exercise = new Schema({
-  text: {
-    type: String,
-    unique: true,
-    required: true
-  },
-  isDone: {
-    type: Boolean,
-    required: true
-  },
+  day: {
+    type: Date,
+    default: Date.now
 
+  },
+  exercises: [{
+
+    name: {
+      type: String,
+      unique: false,
+      required: true
+    },
+    type: {
+      type: String,
+      unique: false,
+      required: true
+    },
+    weight: {
+      type: Number,
+      unique: false,
+      required: false
+    },
+    sets: {
+      type: Number,
+      unique: false,
+      required: false
+    },
+    reps: {
+      type: Number,
+      unique: false,
+      required: false
+    },
+    duration: {
+      type: Number,
+      unique: false,
+      required: true
+    },
+    distance: {
+      type: Number,
+      unique: false,
+      required: false
+    }
+  }]
 }, { timestamps: true })
 
 
